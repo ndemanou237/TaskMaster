92b91ae3d0f4853f808275e2d7d81d64f9ea21b6
ndemanou237 <ndemanoudonald9@gmail.com>
2026-07-14 10:14:18 +0100

    add the contain in the file views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Tache
 
-# Create your views here.
+@login_required
+def liste_tache(request):
+    taches = Tache.objects.filter(utilisateur=request.user).order_by('date_echeance')
+    context = {
+        'taches':taches
+    }
+    return render(request, 'task/dashboard.html', context)
+
+@login_required
+def basculer_tache(request, id):
+    tache = get_object_or_404(Tache, id=id, utilisateur = request.user)
+    tache.marquer_comme_terminee()
+    return redirect('liste_taches')
+
+@login_required
+def ajouter_tache(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'task/ajouter.html')
