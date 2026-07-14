5eb9d76852dd96170305cd063d3fef4e8dbda558
ndemanou237 <ndemanoudonald9@gmail.com>
2026-07-14 09:45:28 +0100

    initialisation the project taskmaster and edit the file models.py

---
+++ tasks/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Categorie(models.Model):
+    name = models.CharField(max_lenght=100)
+    def __str__(self):
+        return self.name
+
+class Tache(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField()
+    date_echeance = models.DateTimeField()
+    est_termine = models.BooleanField(default=False)
+
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
+    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.titre
+    
+    def marquer_comme_terminee(self):
+        self.est_termine = True
+        self.save()
+
+    def est_en_retard(self):
+        return timezone.now () > self.date_echeance and not self.est_termine   
